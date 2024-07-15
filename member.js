63850a363975f46ad9bd544e86a94d91c18a3cff
abijitharun45 <132763981+abijitharun45@users.noreply.github.com>
2024-07-15 11:09:43 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMembers() {
+  const members = [
+    { name: 'John', skills: ['JavaScript', 'React', 'Node'] },
+    { name: 'Jane', skills: ['JavaScript', 'Angular', 'Python'] }
+  ];
+
+  return members;
+}
